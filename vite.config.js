51211ce41d12ff568b56c6c7f2881c67ca1d0f1e
igirb <139497578+igirb@users.noreply.github.com>
2024-09-29 19:00:59 +0200

    Refactor base link

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: 'https://github.com/igirb/diligent-academy-cicd-recipes-selfstudy.git',
+    base: '/diligent-academy-cicd-recipes-selfstudy/',
 });
